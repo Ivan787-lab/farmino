c897780b3589e10030d7ed781be3b679d6b0ed07
Ivan <ivanulanovskij143@gmail.com>
2021-03-07 23:01:17 +0500

    the services page was moved to dist directory

--- dist/pages/services/js/script.js
+++ dist/pages/services/js/script.js
@@ -1,37 +1,4 @@
-$(".middle-block__pagination").slick({
-    arrows: false, 
-    dots: true,
-    speed: 300, 
-    slidesToShow: 1,
-    centerMode: true,
-    variableWidth: true,
-    mobileFirst: true,  
-})  
-
-$('.reviews-block__content-block').slick({
-    prevArrow: "<i class='content-block__prev-btn fas fa-chevron-right'></i>",
-    nextArrow: "<i class='content-block__next-btn fas fa-chevron-right'></i>",
-    mobileFirst: true,
-
+$(".main-container__slider-services").slick({
+    arrows:false,
+    dots:true,
 })
-
-$(".gallery__images").slick({
-    arrows: false,
-    dots: true,
-    speed: 300,
-    slidesToShow: 1,
-    centerMode: true,
-    variableWidth: true,
-    mobileFirst: true,
-})
-
-$(".blog-container__content").slick({
-    arrows: false,
-    dots: true,
-    speed: 300,
-    slidesToShow: 1,
-    centerMode: true,
-    variableWidth: true,
-    mobileFirst: true,
-})
-
