6544e2735163113609b30d194c006ee7f891be9c
Ivan <ivanulanovskij143@gmail.com>
2021-03-07 17:23:07 +0500

    all preparation for creating a page with services is done

---
+++ README.md
@@ -0,0 +1,12 @@
+ # Farmino - проект для тренировки навыков верстки
+ 
+ ## Что используется в проекте?
+ 
+ - Сборщик проектов Gulp
+   - browser-sync
+   - scss-formatter
+   - cache (для сжатия картинок и файлов)
+   - autoprefixer
+- Препроцессор scss
+
+## Ссылка на сам макет => https://www.figma.com/file/xmutHNsorElfpWl65jmcUn/farmino?node-id=0%3A657
---
+++ gulpfile.js
@@ -0,0 +1,117 @@
+const {
+    stream
+} = require('browser-sync');
+const { dest } = require('gulp');
+var gulp = require('gulp'),//npm i gulp --save-dev
+    sass = require('gulp-sass'), //npm i gulp-sass --save-dev
+    browser_sync = require('browser-sync'), //npm i browser-sync --save-dev 
+    //del = require('del'), //npm i del --save-dev
+    imagemin = require('gulp-imagemin'), //npm i gulp-imagemin imagemin-pngquant --save-dev
+    pngquant = require('imagemin-pngquant'), //npm i gulp-imagemin imagemin-pngquant --save-dev
+    cache = require('gulp-cache'), //npm i gulp-cache --save-dev
+    autoprefixer = require('gulp-autoprefixer') //npm i --save-dev gulp-autoprefixer
+
+gulp.task('scss', function () { // Создаем таск "sass"
+    return gulp.src('app/pages/home/css/**/*.scss') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+            cascade: true
+        })) // Создаем префиксы
+        .pipe(gulp.dest('app/pages/home/css')) // Выгружаем результата в папку app/css
+        .pipe(browser_sync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('scss', function () { // Создаем таск "sass"
+    return gulp.src('app/pages/home/css/**/*.scss') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+            cascade: true
+        })) // Создаем префиксы
+        .pipe(gulp.dest('app/pages/home/css')) // Выгружаем результата в папку app/css
+        .pipe(browser_sync.reload({
+            stream: true
+        }))
+});
+
+// вводил gulp scss  
+
+gulp.task('browser_sync', function () {
+    browser_sync({
+        server: {
+            baseDir: 'app/pages/about',
+
+        },
+        notify: false
+    })
+})
+
+gulp.task('html', function () {
+    return gulp.src('app/pages/home/**/*.html')
+        .pipe(browser_sync.reload({
+            stream: true
+        }))
+})
+
+gulp.task('scripts', function () {
+    return gulp.src(['app/pages/home/js/**/*.js', 'app/pages/home/libs**/*.js'])
+        .pipe(browser_sync.reload({
+            stream: true
+        }))
+})
+
+
+gulp.task('watch', function () {
+    gulp.watch('app/pages/home/css/**/*.scss', gulp.parallel('scss'));
+    gulp.watch('app/pages/home/**/*.html', gulp.parallel('html'))
+    gulp.watch(['app/pages/home/js/**/*.js', 'app/pages/home/libs/**/*.js'], gulp.parallel('scripts'))
+})
+
+//вводил gulp watch
+
+
+//для итоговой папки dist
+
+/*gulp.task('clean', function () {
+    return del.sync('dist/')// удалит папку dist
+})*/
+
+gulp.task('img', function () {
+    return gulp.src('app/pages/home/img/**/*')
+        .pipe(cache(imagemin({
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{
+                removeViewBox: false
+            }],
+            use: [pngquant()]
+        })))
+        .pipe(dest('dist/home/img'))
+})
+
+gulp.task('prebuild', async function () {
+    let buildCss = gulp.src([
+        'app/pages/home/css/**/*.css',
+        'app/pages/home/css/**/*.scss',
+    ])
+        .pipe(gulp.dest('dist/home/css'))
+
+    let buildJs = gulp.src('app/pages/home/js/**/*.js')
+        .pipe(gulp.dest('dist/home/js'));
+
+    let buildHtml = gulp.src('app/pages/home/**/*.html')
+        .pipe(gulp.dest('dist/home'));
+
+    let buildLibs = gulp.src("app/pages/home/libs/**/*")
+        .pipe(gulp.dest("dist/home/libs"))
+})
+
+gulp.task('clear', function () {
+    return cache.clearAll()//удалит кеш
+})
+
+
+gulp.task('default', gulp.parallel('scss', 'scripts', 'browser_sync', 'watch'))
+gulp.task('build', gulp.parallel('prebuild', 'img', 'scss', 'scss', 'scripts'))
+//gulp build
