c1cd707906cee9d54a8ca72d7ee8221a15f41052
Ivan <ivanulanovskij143@gmail.com>
2021-03-07 22:44:05 +0500

    work on the service page was moved to dist directory

---
+++ dist/pages/services/js/script.js
@@ -0,0 +1,37 @@
+$(".middle-block__pagination").slick({
+    arrows: false, 
+    dots: true,
+    speed: 300, 
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    mobileFirst: true,  
+})  
+
+$('.reviews-block__content-block').slick({
+    prevArrow: "<i class='content-block__prev-btn fas fa-chevron-right'></i>",
+    nextArrow: "<i class='content-block__next-btn fas fa-chevron-right'></i>",
+    mobileFirst: true,
+
+})
+
+$(".gallery__images").slick({
+    arrows: false,
+    dots: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    mobileFirst: true,
+})
+
+$(".blog-container__content").slick({
+    arrows: false,
+    dots: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    mobileFirst: true,
+})
+
