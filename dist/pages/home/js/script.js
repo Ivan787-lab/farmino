d7f0b95c665122dd9ccf0312ca83332923887650
Ivan <ivanulanovskij143@gmail.com>
2021-03-07 17:55:44 +0500

    work on the home page completed

--- dist/pages/home/js/script.js
+++ dist/pages/home/js/script.js
@@ -7,7 +7,7 @@ $(".middle-block__pagination").slick({
     variableWidth: true,
     mobileFirst: true,  
 })  
-
+ 
 $('.reviews-block__content-block').slick({
     prevArrow: "<i class='content-block__prev-btn fas fa-chevron-right'></i>",
     nextArrow: "<i class='content-block__next-btn fas fa-chevron-right'></i>",
