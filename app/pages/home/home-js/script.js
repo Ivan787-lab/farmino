5f551ec3d97512abd07369fa9e4a97262e3534c9
Ivan <ivanulanovskij143@gmail.com>
2021-03-10 12:41:05 +0500

    removed one block

--- app/pages/home/home-js/script.js
+++ app/pages/home/home-js/script.js
@@ -8,12 +8,7 @@ $(".middle-block__pagination").slick({
     mobileFirst: true,  
 })  
 
-$('.reviews-block__content-block').slick({
-    prevArrow: "<i class='content-block__prev-btn fas fa-chevron-right'></i>",
-    nextArrow: "<i class='content-block__next-btn fas fa-chevron-right'></i>",
-    mobileFirst: true,
 
-})
 
 $(".gallery__images").slick({
     arrows: false,
